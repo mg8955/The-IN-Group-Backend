3dd585baeeac32681fd2cf6339b7bc31de31c1d9
Michael Gostomski <gostomski.m@gmail.com>
2022-11-23 09:10:14 -0600

    added model pseudocode, set up server, config and routing indexes

---
+++ models/Thought.js
@@ -0,0 +1,37 @@
+// THOUGHTTEXT
+// String
+// Required
+// between 1-280 char
+
+// CREATEDAT
+// Date object
+// default: current timestamp
+
+// USERNAME
+// String
+// Required
+
+// REACTIONS
+// Array of nested docs created with REACTION Schema
+
+// VIRTUAL reactionCOUNT
+// number of reactions on a thought reaction array
+
+// REACTION SCHEMA
+// reactionID
+// use ObjectId data type
+// default set to new ObjectId
+
+// REACTIOBODY
+// String
+// Required
+// between 1-280 char
+
+// Username
+// String
+// Required
+
+// createdAt
+// Date object
+// default: current timestamp
+// getter method to format timestampt on query
---
+++ models/User.js
@@ -0,0 +1,20 @@
+// Username
+// String
+// Unique
+// Required
+// Trimmed
+
+// EMAIL
+// String
+// Required
+// Unique
+// email validation via mongoose
+
+// THOUGHTS
+// Schema Array references THOUGHTS Schema
+
+// FRIENDS
+// Schema Array references User model
+
+// VIRTUAL
+// friendcount
